d8f1d75ec15a54ad5771f20dc3a60bf4e76d11f4
Marisol Pena <masopego@gmail.com>
2021-11-13 11:39:15 +0100

    Add images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import logoImg from "./content/logo_1.png";
+
 console.log("Look ma! Zero config");
+
+const img = document.createElement("img");
+img.src = logoImg;
+
+document.getElementById("imgContainer").appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const basePath = __dirname;
 
 module.exports = {
   mode: "development",
+  context: path.join(basePath, "src"),
   entry: {
-    app: "./src/index.js",
-    appStyles: ["./src/styles.css"],
+    app: "./index.js",
+    appStyles: ["./styles.scss"],
   },
   output: {
     filename: "[name].[chunkhash].js",
@@ -21,9 +23,31 @@ module.exports = {
         loader: "babel-loader",
       },
       {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
       },
     ],
   },
@@ -33,7 +57,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "./src/index.html",
+      template: "./index.html",
       scriptLoading: "blocking",
     }),
     new CleanWebpackPlugin(),
